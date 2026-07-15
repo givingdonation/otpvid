7b829b5f8d8b4e0e2f0ecff106a08a2346bf43ee
givingdonation <me@fano.club>
2026-07-15 00:36:18 -0700

    add to video until plaintext part

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class OtpVid(VoiceoverScene):
 
         aliceup = Text("alice", font="Xanmono").to_edge(UP)
         bobdown = Text("bob", font="Xanmono").to_edge(DOWN)
-        
+        #open_mail = SVGMobject("mail_open.svg")
         with self.voiceover(text="If you want to send your friend a message that is impossible for anyone else to read, do this.") as tracker:
             self.play(AddTextLetterByLetter(aliceup))
             self.play(AddTextLetterByLetter(bobdown))
@@ -40,16 +40,56 @@ class OtpVid(VoiceoverScene):
             self.play(FadeIn(closed))
             self.play(closed.animate.to_edge(DOWN).shift(UP), run_time=1.5)
             self.play(FadeOut(closed))
-        #open_mail = SVGMobject("mail_open.svg")
-
-
-            self.wait()
+            #self.wait()
             self.play(FadeOut(VGroup(aliceup,bobdown)))
         key = Text("3150704", font="Xanmono")
-        self.play(AddTextLetterByLetter(key))
-        self.play(FadeOut(key))
-        dice = ImageMobject("dice.png").scale(0.25).to_edge(UP)
-        self.play(FadeIn(dice))
+        with self.voiceover(text="Start by making a truly random digit code.") as tracker:
+            
+            self.play(AddTextLetterByLetter(key), run_time=tracker.duration*0.75)
+            self.play(FadeOut(key), run_time=tracker.duration*0.25)
+        with self.voiceover(text="You could use dice and a table like this one."):
+            dice = ImageMobject("dice.png").scale(0.25).to_edge(UP)
+            self.play(FadeIn(dice))
+            board = ImageMobject("diceboard.png").scale_to_fit_width(config.frame_width).to_edge(DOWN)
+            self.play(FadeIn(board))
+            self.play(FadeOut(Group(dice,board)))
+        keyIcon = SVGMobject("keyicon.svg")
+        onetime = ImageMobject("onetime.png").scale_to_fit_width(config.frame_width).to_edge(DOWN)
+        
+        with self.voiceover(text="This code is the key you can only use once.") as tracker:
+            self.play(AddTextLetterByLetter(key.to_edge(UP)))
+            self.play(FadeIn(keyIcon.to_edge(UP).shift(DOWN)))
+            self.play(FadeIn(onetime))
+            #self.play(FadeOut(key))
+            self.play(FadeOut(Group(key,keyIcon,onetime)))
+
+
+        with self.voiceover(text="You and your friend should memorize or safely store this.") as tracker:
+            self.play(FadeIn(keyIcon.move_to(ORIGIN)))
+            box = SurroundingRectangle(keyIcon, color=WHITE)
+            self.play(FadeIn(box))
+            self.play(FadeOut(box),FadeOut(keyIcon))
+        
+        plaintext = Text("GO EAST", font="Xanmono")
+        plaincode = Text("6603457", font="Xanmono")
+
+        with self.voiceover(text="When you want to send a message to your friend, first turn your message into a digit code.") as tracker:
+            self.play(AddTextLetterByLetter(plaintext))
+
+
+
+
+        #with self.voiceover(text="") as tracker:
+
+
+
+        
+
+
+            
+
+            
+        
 
 
 
